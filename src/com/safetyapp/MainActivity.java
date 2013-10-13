0fb69cbe2f64cdc27ed6a44f7a6b3eb5731d6e58
Jack Miller <miller.4999@osu.edu>
2013-10-13 12:35:27 -0400

    Added google map.

--- src/com/safetyapp/MainActivity.java
+++ src/com/safetyapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.safetyapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +21,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void showMap(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
     
 }
